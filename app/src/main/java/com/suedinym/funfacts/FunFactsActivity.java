3006181fc22e119a3218f82dbcc2de4985e81317
sue <suedinym@gmail.com>
2015-08-26 08:05:46 -0400

    Changed FunFacts to FunFactsActivity in javacode so it would match the video

--- app/src/main/java/com/suedinym/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/suedinym/funfacts/FunFactsActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class FunFacts extends Activity {
+public class FunFactsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
